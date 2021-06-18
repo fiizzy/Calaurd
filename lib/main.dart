7d912d7d706e340ba509d8bc1a72e7841b9c3be9
Fisayo Obilaja <fisayoobilaja@Fisayos-MacBook-Pro.local>
2021-06-18 20:54:31 +0100

    Debugged on iOS and removed firebase for now

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,11 @@ import 'package:calaurd/views/onboarding/onboarding.dart';
 import 'package:calaurd/views/uploadImage/uploadImage.dart';
 import 'providers/imageProvider.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  
   runApp(MyApp());
 }
 
